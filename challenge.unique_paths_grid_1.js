cd1fc51d93a9ce5329dc6d9eb21af17daa1244df
The Right Honourable Reverend <samueljsmith@gmail.com>
2022-02-01 02:41:23 -0600

    Update challenge.unique_paths_grid_1.js

--- challenge.unique_paths_grid_1.js
+++ challenge.unique_paths_grid_1.js
@@ -5,7 +5,7 @@ export const solve = (params) => {
     return solution;
 };
 
-export async function attempt(params) {
+export function attempt(params) {
     let test = solve([3,7]);
     if (test !== 28) { throw "Test 1 failed to pass"; }
     
