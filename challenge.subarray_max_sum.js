0a990633aebc68a2ec57a553bb88c8bbabe29007
The Right Honourable Reverend <samueljsmith@gmail.com>
2022-02-01 02:51:58 -0600

    Update challenge.subarray_max_sum.js

--- challenge.subarray_max_sum.js
+++ challenge.subarray_max_sum.js
@@ -1,11 +1,11 @@
 
 // add your code here.
-export const solve = (params) => {
+const solve = (params) => {
     let solution = null;
     return solution;
 };
 
-export function attempt(params) {
+function attempt(params) {
     let test = solve([-2,1,-3,4,-1,2,1,-5,4]);
     if (test !== 6) { throw "Test 1 failed to pass"; }
     
@@ -22,3 +22,7 @@ export function attempt(params) {
 // note, challenges are much easier to solve in an IDE.
 // working in an IDE, you'll want to remove the exports.
 // without an IDE, you can add a main(ns) statement to test your solutions
+export function attempt(params) {
+    return tests(params);
+}
+
