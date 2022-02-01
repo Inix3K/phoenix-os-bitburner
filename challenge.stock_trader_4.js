8774a7f4ae04d399957fbe473c3b4ae7a7c78d9a
The Right Honourable Reverend <samueljsmith@gmail.com>
2022-02-01 02:41:03 -0600

    Update challenge.stock_trader_4.js

--- challenge.stock_trader_4.js
+++ challenge.stock_trader_4.js
@@ -5,7 +5,7 @@ export const solve = (params) => {
     return solution;
 };
 
-export async function attempt(params) {
+export function attempt(params) {
     let test = solve([2, [2,4,1]]);
     if (test !== 2) { throw "Test 1 failed to pass"; }
     
