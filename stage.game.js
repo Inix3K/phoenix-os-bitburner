b1010d6768a0861692a587e36085a0c657a57f7f
Sage Smith <sage@limitlessinteractive.com>
2022-02-01 08:02:04 -0600

    removing deprecated files

--- stage.game.js
+++
@@ -1,9 +0,0 @@
-import { LogicState } from "./lib.gamestates.so";
-/**
- * @typedef {import(".").NS} ns
- * @typedef {import("./phoenix-doc").PlayerObject} PlayerObject
- * @typedef {import("./phoenix-doc").ServerObject} ServerObject
- *
- */
-
-
--- stage.money.js
+++
@@ -1,7 +0,0 @@
-/**
- * @typedef {import(".").NS} ns
- * @typedef {import("./phoenix-doc").PlayerObject} PlayerObject
- * @typedef {import("./phoenix-doc").ServerObject} ServerObject
- *
- */
-
