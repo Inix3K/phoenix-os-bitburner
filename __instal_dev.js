bb3a07cc55864c36c812e0453822734e94bff4b8
Sage Smith <sage@limitlessinteractive.com>
2022-02-01 08:19:12 -0600

    install script for dev branch

---
+++ __instal_dev.js
@@ -0,0 +1,86 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/__uninstall.js","__uninstall.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/bin.gr.future.js","bin.gr.future.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/bin.gr.futureloop.js","bin.gr.futureloop.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/bin.gr.loop.js","bin.gr.loop.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/bin.gr.once.js","bin.gr.once.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/bin.hk.future.js","bin.hk.future.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/bin.hk.futureloop.js","bin.hk.futureloop.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/bin.hk.loop.js","bin.hk.loop.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/bin.hk.once.js","bin.hk.once.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/bin.prep.js","bin.prep.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/bin.share.loop.js","bin.share.loop.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/bin.universal.loop.js","bin.universal.loop.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/bin.wk.future.js","bin.wk.future.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/bin.wk.futureloop.js","bin.wk.futureloop.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/bin.wk.loop.js","bin.wk.loop.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/bin.wk.once.js","bin.wk.once.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/etc.delete_server.js","etc.delete_server.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/etc.motd.js","etc.motd.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/etc.purchase_server.js","etc.purchase_server.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/etc.x11.js","etc.x11.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/fake.corps.js","fake.corps.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/fake.crimes.js","fake.crimes.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/fake.factions.js","fake.factions.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/fake.leetcode.js","fake.leetcode.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/fake.singularity.js","fake.singularity.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/fake.sleeve.js","fake.sleeve.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/lib.augmentations.so.js","lib.augmentations.so.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/lib.database.so.js","lib.database.so.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/lib.factions.so.js","lib.factions.so.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/lib.gamestates.so.js","lib.gamestates.so.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/lib.loader.so.js","lib.loader.so.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/lib.player.so.js","lib.player.so.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/lib.server.so.js","lib.server.so.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/lib.serverextras.so.js","lib.serverextras.so.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/lib.structures.so.js","lib.structures.so.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/lib.utils.so.js","lib.utils.so.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/logic.corps.js","logic.corps.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/logic.crimes.js","logic.crimes.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/logic.factions.js","logic.factions.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/logic.hack.js","logic.hack.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/logic.leetcode.js","logic.leetcode.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/logic.money.js","logic.money.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/logic.singularity.js","logic.singularity.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/logic.sleeve.js","logic.sleeve.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/phoenix.js","phoenix.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/sbin.hnet.js","sbin.hnet.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/sbin.keepalive.js","sbin.keepalive.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/sbin.market.js","sbin.market.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/sbin.scp.js","sbin.scp.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/sbin.sudo.js","sbin.sudo.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/src.prettytable.js","src.prettytable.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/strategy.base.js","strategy.base.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/strategy.efficiency.js","strategy.efficiency.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/strategy.hwgw.js","strategy.hwgw.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/strategy.hwgw2.js","strategy.hwgw2.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/strategy.maxcash.js","strategy.maxcash.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/strategy.nohacks.js","strategy.nohacks.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/strategy.override.txt","strategy.override.txt");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/strategy.rep.js","strategy.rep.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/tucson.js","tucson.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/usr.contracts.js","usr.contracts.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/usr.fixcommonbugs.js","usr.fixcommonbugs.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/usr.list.js","usr.list.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/usr.route.js","usr.route.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/usr.sf4route.js","usr.sf4route.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/var.constants.js","var.constants.js");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/usr.aliases.txt","usr.aliases.txt");
+    await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/dev/var.debug.txt","var.debug.txt");
+    
+
+    ns.tprint("Phoenix OS kernel v1.0.0 has been loaded into ", ns.getHostname());
+    ns.tprint("You can start Phoenix by running phoenix.js or sbin.keepalive.js");
+    if (await ns.prompt("Phoenix OS has been installed. Would you like to launch now?")) {
+        if (ns.getServerMaxRam("home") <= 16) {
+            if (await ns.prompt("Our hyper-advanced AI has determined that your RAM is insufficient. Run Phoenix lite?"))
+                ns.tprint("Starting Tucson in 10s...");
+                ns.spawn('tucson.js');
+        } else {
+            ns.tprint("Starting Phoenix in 10s...");
+            ns.spawn('phoenix.js');
+        }
+        
+    }
+}
