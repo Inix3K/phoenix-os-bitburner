fe0e6cf1b7d7fc2b7e2be0a9a86778af140c9a7d
Sage Smith <sage@limitlessinteractive.com>
2022-02-02 11:33:33 -0600

    added w eighted edges to faction graph

--- var.constants.js
+++ var.constants.js
@@ -1,8 +1,7 @@
 export const reservedHomeRam = 64;
-export const loop_time = 3000;
+export const loop_time = 12000;
 
-const HackSemaphores = {
-};
+const HackSemaphores = {};
 
 export const strategy_semaphores = HackSemaphores;
 
@@ -16,6 +15,7 @@ export const faction_blockers = new Map([
     ["NiteSec", []],
     ["CyberSec", []],
     ["Tian Di Hui", []],
+    ["Netburners", []],
     ["BitRunners", []],
     ["ECorp", []],
     ["The Black Hand", []],
@@ -39,9 +39,39 @@ export const faction_blockers = new Map([
     ["Daedalus", []]
 ]);
 
-
-
-
+export const faction_edges = new Map([
+    ["Sector-12", {type: "faction", req: 0}],
+    ["Chongqing", {type: "faction", req: 0}],
+    ["New Tokyo", {type: "faction", req: 0}],
+    ["Ishima", {type: "faction", req: 0}],
+    ["Aevum", {type: "faction", req: 0}],
+    ["Volhaven", {type: "faction", req: 0}],
+    ["NiteSec", {type: "faction", req: 220}],
+    ["CyberSec", {type: "faction", req: 70}],
+    ["Tian Di Hui", {type: "faction", req: 0}],
+    ["Netburners", {type: "faction", req: 220}],
+    ["BitRunners", {type: "faction", req: 550}],
+    ["ECorp", {type: "company", req: 200000}],
+    ["The Black Hand", {type: "faction", req: 350}],
+    ["KuaiGong International", {type: "company", req: 200000}],
+    ["MegaCorp", {type: "company", req: 200000}],
+    ["NWO", {type: "company", req: 200000}],
+    ["Four Sigma", {type: "company", req: 200000}],
+    ["OmniTek Incorporated", {type: "company", req: 200000}],
+    ["Blade Industries", {type: "company", req: 200000}],
+    ["Clarke Incorporated", {type: "company", req: 200000}],
+    ["Bachman & Associates", {type: "company", req: 200000}],
+    ["Slum Snakes", {type: "gang", req: 9}],
+    ["Fulcrum Secret Technologies", {type: "company", req: 250000}],
+    ["Silhouette", {type: "gang", req: 200000}],
+    ["Tetrads", {type: "gang", req: 18}],
+    ["The Dark Army", {type: "gang", req: 45}],
+    ["Speakers for the Dead", {type: "gang", req: 45}],
+    ["The Covenant", {type: "gang", req: 250000}],
+    ["The Syndicate", {type: "gang", req: 50000}],
+    ["Illuminati", {type: "gang", req: 450000}],
+    ["Daedalus", {type: "gang", req: 250000}]
+]);
 
 
 
